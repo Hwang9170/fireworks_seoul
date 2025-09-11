78c386f9d866a9fa3ac88f8d53134a6e539cc549
Hwang9170 <jkimg9170@naver.com>
2025-09-11 20:17:09 +0900

    insta

--- script.js
+++ script.js
@@ -50,3 +50,24 @@ function autoSlide() {
         plusSlides(1);
     }, 5000); // 5초마다 슬라이드 변경
 }
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.opacity = "0";
+        slides[i].style.transition = "opacity 1s ease";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(() => { slides[slideIndex-1].style.opacity = "1"; }, 50);
+    dots[slideIndex-1].className += " active";
+}
